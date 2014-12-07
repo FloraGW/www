434b1d9f19ac66e3b8a33e3232ae5a2dcfa56d78
Flora <flora@flora.flora>
2014-12-07 14:13:41 -0500

    Continuation de vue/commun

--- RiseOfMontShaff/index.php
+++ RiseOfMontShaff/index.php
@@ -2,11 +2,6 @@
 
 <?php include("vue/commun/menu.php"); ?>
 
-Bonjour toto ! Bonjour toto !
-<p><br><br><br>Bonjour toto ! Bonjour toto !
-
-<?php 
-//include("includes/menus.php");
-?>
+<?php include("vue/commun/menu.php"); ?>
 
 <?php include("vue/commun/footer.php"); ?>
--- RiseOfMontShaff/vue/commun/header.php
+++ RiseOfMontShaff/vue/commun/header.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>Rise of MontShäff</title>
+        <title>The Rise of Mont-Shäff</title>
         <link rel="stylesheet" type="text/css" href="vue/commun/style.css">
     </head>
  
@@ -13,5 +13,9 @@
     <header>
     	<div class="header">
        		Rise of Montshäff
+       	</div>
+       	<div class="titre">
+       		<b>THE RISE OF
+       		<br>MONT-SHÄFF</b>
        	</div>
     </header>
