3a8d13857f16690af320b017dfe787c1f129c2dd
LeBrave <lebrave@lebrave.lebrave>
2014-12-09 04:49:07 -0500

    Mise à jour du script de base de données.

--- RiseOfMontShaff/riseofmontshaff.sql
+++ RiseOfMontShaff/riseofmontshaff.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 09 Décembre 2014 à 06:13
+-- Généré le :  Mar 09 Décembre 2014 à 10:47
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -58,10 +58,8 @@ CREATE TABLE IF NOT EXISTS `fil` (
 --
 
 INSERT INTO `fil` (`noFil`, `noCategorie`, `Nom`) VALUES
-(2, 5, 'Test1'),
-(3, 5, 'Test2'),
-(4, 5, 'YAHOO'),
-(5, 5, 'Test3');
+(2, 5, 'Premier test'),
+(4, 5, 'YAHOO / GOOGLE');
 
 -- --------------------------------------------------------
 
@@ -75,7 +73,7 @@ CREATE TABLE IF NOT EXISTS `nouvelle` (
   `Contenu` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `DateCreation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`noNouvelle`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 --
 -- Contenu de la table `nouvelle`
@@ -84,8 +82,30 @@ CREATE TABLE IF NOT EXISTS `nouvelle` (
 INSERT INTO `nouvelle` (`noNouvelle`, `Titre`, `Contenu`, `DateCreation`) VALUES
 (1, 'Ouverture !', 'C''est la grande ouverture du site officiel du jeu vidéo le plus attendu de l''année : The Rise of Mont-Shäff !!!', '2014-12-07 23:39:10'),
 (2, 'Viva la résolution !', 'Chers joueurs, chères joueuses,\r\n\r\nLa conception du jeu va bon train. Nous vous remercions d''être en si grand nombre à suivre le développement de The Rise of Mont-Shäff, le jeu qui va révolutionner le monde.', '2014-12-08 00:50:14'),
-(3, 'Igloo Igloo Igloo !!!', 'L''Igloo du village, la fameuse mascotte de notre équipe, a besoin d''aller faire ses petits besoins.\r\n\r\nNous devons donc nous absenter, mais ne vous inquiéter point, toute l''équipe de MicroShäff sera de retour dans seulement quelques minutes... Ou plutôt, quelques minutes après notre départ pour le monde extérieur... qui aura lieu... euh... nous ne le savons même pas...\r\n\r\nÀ suivre !', '2014-12-08 00:57:54'),
-(4, 'Iñtërnâtiônàlizætiøn', 'Iñtërnâtiônàlizætiøn', '2014-12-08 02:56:39');
+(4, 'Iñtërnâtiônàlizætiøn !', 'Iñtërnâtiônàlizætiøn QUE ES ? UNA MODIFICACION DEL PATRA !', '2014-12-08 02:56:39'),
+(5, 'La toute première nouvelle entrée par le site', 'TESTONS ET HOURRA !', '2014-12-09 07:23:12');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `photo`
+--
+
+CREATE TABLE IF NOT EXISTS `photo` (
+  `noPhoto` int(11) NOT NULL AUTO_INCREMENT,
+  `Chemin` varchar(255) NOT NULL,
+  PRIMARY KEY (`noPhoto`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Contenu de la table `photo`
+--
+
+INSERT INTO `photo` (`noPhoto`, `Chemin`) VALUES
+(1, 'vue/galerie/image/art1.jpg'),
+(2, 'vue/galerie/image/2.jpg'),
+(3, 'vue/galerie/image/art2.jpg'),
+(4, 'vue/galerie/image/art3.jpg');
 
 -- --------------------------------------------------------
 
@@ -100,7 +120,7 @@ CREATE TABLE IF NOT EXISTS `post` (
   `Contenu` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `DateCreation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`noPost`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
 
 --
 -- Contenu de la table `post`
@@ -111,7 +131,8 @@ INSERT INTO `post` (`noPost`, `noFil`, `noUtilisateur`, `Contenu`, `DateCreation
 (3, 4, 1, 'testons', '2014-12-09 00:29:10'),
 (4, 4, 1, 'Testons encore !! :D', '2014-12-09 00:29:20'),
 (5, 4, 2, 'Dieu&lt;', '2014-12-09 00:29:46'),
-(6, 2, 5, 'Ouuuuuaaaaiiiiisssss !!!', '2014-12-09 00:33:58');
+(7, 2, 5, 'On essaie deux trois trucs !\r\nHourra !', '2014-12-09 06:22:14'),
+(8, 2, 2, 'Dieu est partout', '2014-12-09 06:22:58');
 
 -- --------------------------------------------------------
 
@@ -137,6 +158,27 @@ INSERT INTO `utilisateur` (`noUtilisateur`, `Nom`, `MotDePasse`, `Role`, `Avatar
 (2, 'Dieu', 'dieu', 'Admin', 'vue/utilisateur/image/Dieu.jpg'),
 (3, 'Flora', '1234', 'Admin', 'vue/utilisateur/image/Flora.jpg'),
 (5, 'test', 'test', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `video`
+--
+
+CREATE TABLE IF NOT EXISTS `video` (
+  `noVideo` int(11) NOT NULL AUTO_INCREMENT,
+  `Code` varchar(25) NOT NULL,
+  PRIMARY KEY (`noVideo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `video`
+--
+
+INSERT INTO `video` (`noVideo`, `Code`) VALUES
+(1, 'W7Dcev81AIM'),
+(2, 'Av5ZMqj15XE'),
+(3, 'CsGYh8AacgY');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
