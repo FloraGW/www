c7c2fb7e5ecc5adb341a7a67bf4f6c305c48e2ad
LeBrave <lebrave@lebrave.lebrave>
2014-12-09 12:59:39 -0500

    Réparation d'un bug si la base de données était vide

--- RiseOfMontShaff/controleur/accueil/index.php
+++ RiseOfMontShaff/controleur/accueil/index.php
@@ -1,33 +1,54 @@
 <?php
 include_once('modele/accueil/getLatestNouvelle.php');
 $nouvelle = getLatestNouvelle();
-$nouvelle['titre'] = htmlspecialchars($nouvelle['titre'], ENT_SUBSTITUTE, "");
-$nouvelle['contenu'] = htmlspecialchars($nouvelle['contenu'], ENT_SUBSTITUTE, "");
+if($nouvelle != false)
+{
+	$nouvelle['titre'] = htmlspecialchars($nouvelle['titre'], ENT_SUBSTITUTE, "");
+	$nouvelle['contenu'] = htmlspecialchars($nouvelle['contenu'], ENT_SUBSTITUTE, "");
+}
 
 include_once('modele/accueil/getLatestPost.php');
 $post = getLatestPost();
-$post['contenu'] = htmlspecialchars($post['contenu'], ENT_SUBSTITUTE, "");
+if($post != false)
+{
+	$post['contenu'] = htmlspecialchars($post['contenu'], ENT_SUBSTITUTE, "");
+}
 
 include_once('modele/utilisateur/getUtilisateur.php');
 $utilisateur = getUtilisateur($post['noUtilisateur']);
-$utilisateur['nom'] = htmlspecialchars($utilisateur['nom'], ENT_SUBSTITUTE, "");
-$utilisateur['role'] = htmlspecialchars($utilisateur['role'], ENT_SUBSTITUTE, "");
+if($utilisateur != false)
+{
+	$utilisateur['nom'] = htmlspecialchars($utilisateur['nom'], ENT_SUBSTITUTE, "");
+	$utilisateur['role'] = htmlspecialchars($utilisateur['role'], ENT_SUBSTITUTE, "");
+}
 
 include_once('modele/forum/fils/getFil.php');
 $filPost = getFil($post['noFil']);
-$filPost['nom'] = htmlspecialchars($filPost['nom'], ENT_SUBSTITUTE, "");
+if($filPost != false)
+{
+	$filPost['nom'] = htmlspecialchars($filPost['nom'], ENT_SUBSTITUTE, "");
+}
 
 include_once('modele/forum/categories/getCategorie.php');
 $categoriePost = getCategorie($filPost['noCategorie']);
-$categoriePost['nom'] = htmlspecialchars($categoriePost['nom'], ENT_SUBSTITUTE, "");
+if($categoriePost != false)
+{
+	$categoriePost['nom'] = htmlspecialchars($categoriePost['nom'], ENT_SUBSTITUTE, "");
+}
 
 include_once('modele/accueil/getLatestVideo.php');
 $video = getLatestVideo();
-$video['code'] = htmlspecialchars($video['code'], ENT_SUBSTITUTE, "");
+if($video != false)
+{
+	$video['code'] = htmlspecialchars($video['code'], ENT_SUBSTITUTE, "");
+}
 
 include_once('modele/accueil/getLatestPhoto.php');
 $photo = getLatestPhoto();
-$photo['chemin'] = htmlspecialchars($photo['chemin'], ENT_SUBSTITUTE, "");
+if($photo != false)
+{
+	$photo['chemin'] = htmlspecialchars($photo['chemin'], ENT_SUBSTITUTE, "");
+}
 
 include_once('vue/accueil/index.php');
 ?>
